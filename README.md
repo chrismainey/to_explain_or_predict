20589909c5d60f14c489b5656b0af8edd7b290d2
Chris Mainey <c.mainey1@nhs.net>
2024-11-09 14:26:17 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# RPysoc Conference 2024
+
+## To Explain or Predict: 
+### how different modelling objectives change how you use the same tools
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This is a repository for my talk at RPysoc Conference 2024.
+
+It leans heavily on the work of Professor Galit Smuelli, and her paper of the same name:
+
+Shmueli, G. (2010), To Explain or To Predict?, Statistical Science, vol 25 no 3, pp. 289-310.
+
+https://www.jstor.org/stable/41058949
+
---
+++ working_r.R
@@ -0,0 +1,79 @@
+library(readr)
+heart_failure_clinical_records_dataset <- read_csv("data/heart_failure_clinical_records_dataset.csv")
+
+Hmisc::describe(heart_failure_clinical_records_dataset)
+
+
+heart_failure_clinical_records_dataset$anaemia <- factor(heart_failure_clinical_records_dataset$anaemia)
+heart_failure_clinical_records_dataset$diabetes <- factor(heart_failure_clinical_records_dataset$diabetes)
+heart_failure_clinical_records_dataset$high_blood_pressure <- factor(heart_failure_clinical_records_dataset$high_blood_pressure)
+heart_failure_clinical_records_dataset$sex <- factor(heart_failure_clinical_records_dataset$sex)
+heart_failure_clinical_records_dataset$smoking <- factor(heart_failure_clinical_records_dataset$smoking)
+
+heart_failure_clinical_records_dataset$sc_age <- scale(heart_failure_clinical_records_dataset$age)
+heart_failure_clinical_records_dataset$sc_serum_creatinin <- scale(heart_failure_clinical_records_dataset$serum_creatinine)
+heart_failure_clinical_records_dataset$sc_ejection_fraction <- scale(heart_failure_clinical_records_dataset$ejection_fraction)
+
+model1 <- glm(DEATH_EVENT ~ serum_creatinine + ejection_fraction + age,
+  data=heart_failure_clinical_records_dataset, family = "binomial")
+
+
+summary(model1)
+
+library(ModelMetrics)
+
+auc(model1)
+
+
+library(caret)
+
+
+library(caret)
+set.seed(3456)
+trainIndex <- createDataPartition(heart_failure_clinical_records_dataset$DEATH_EVENT, p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+
+Train <- heart_failure_clinical_records_dataset[ trainIndex,]
+Test  <- heart_failure_clinical_records_dataset[-trainIndex,]
+
+fitControl <- trainControl(## 10-fold CV
+  method = "cv",
+  number = 5)
+
+
+ridge <- train(DEATH_EVENT ~ serum_creatinine + ejection_fraction, data = Train, 
+                 method = 'ridge', 
+                 #lambda1=,
+                 #trControl = fitControl,
+                 family = "binomial",
+                 ## This last option is actually one
+                 ## for gbm() that passes through
+                 verbose = FALSE)
+ridge
+
+
+
+x <- model.matrix(DEATH_EVENT~sc_serum_creatinin+sc_ejection_fraction, Train)[,-1]
+y <- Train$DEATH_EVENT
+
+library(glmnet)
+cv <- cv.glmnet(x, y, alpha = 0, family="binomial")
+# Display the best lambda value
+
+
+ridge1<-glmnet(x,y, alpha=0, lamda=cv$lambda.min, family="binomial")
+
+summary(ridge1)
+
+coef(ridge1)
+
+
+# Make predictions on the test data
+x.test <- model.matrix(DEATH_EVENT~sc_serum_creatinin+sc_ejection_fraction, Test)[,-1]
+
+predictions <- predict(ridge1, newx=x.test, type="response") |> as.vector()
+# Model performance metrics
+
+ModelMetrics::auc(Test$DEATH_EVENT, predictions)
+
